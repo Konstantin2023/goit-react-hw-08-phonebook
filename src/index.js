430f656978d2f6ae71ec521ba93a7bd2d0fab769
Konstantin2023 <pscosx@gmail.com>
2023-04-22 14:02:28 +0300

    Моя дз8

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { App } from 'components/App/App';
+import { store, persistor } from 'redux/store';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'contants/Thema';
+import { GlobalStyle } from 'styles/GlobalStyle';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </ThemeProvider>
+);
